ec83432b5c2b5b9d0aadfdc25e22527352dc5db3
MX <mxber2022@gmail.com>
2025-06-26 19:01:39 +0700

    ccip router address

---
+++ ccipbird/src/utils/routeraddress.ts
@@ -0,0 +1,8 @@
+export const ROUTER_ADDRESSES: Record<string, string> = {
+  arbitrumSepolia: "0x2a9C5afB0d0e4BAb2BCdaE109EC4b0c4Be15a165",
+  baseSepolia: "0xD3b06cEbF099CE7DA4AcCf578aaebFDBd6e88a93",
+  polygonAmoy: "0x9C32fCB86BF0f4a1A8921a9Fe46de3198bb884B2",
+  sepolia: "0x0BF3dE8c5D3e8A2B34D2BEeB17ABfCeBaf363A59",
+};
+
+export const CCIP_ROUTER_ADDRESSES = Object.keys(ROUTER_ADDRESSES);
