79f2d9dc8c1ebeb8dbb818df00224e0d778b8829
MX <mxber2022@gmail.com>
2025-06-26 19:09:32 +0700

    Update routeraddress.ts

--- ccipbird/src/utils/routeraddress.ts
+++ ccipbird/src/utils/routeraddress.ts
@@ -1,8 +1,24 @@
-export const ROUTER_ADDRESSES: Record<string, string> = {
-  arbitrumSepolia: "0x2a9C5afB0d0e4BAb2BCdaE109EC4b0c4Be15a165",
-  baseSepolia: "0xD3b06cEbF099CE7DA4AcCf578aaebFDBd6e88a93",
-  polygonAmoy: "0x9C32fCB86BF0f4a1A8921a9Fe46de3198bb884B2",
-  sepolia: "0x0BF3dE8c5D3e8A2B34D2BEeB17ABfCeBaf363A59",
+export const CHAIN_ADDRESSES: Record<string, { router: string; usdc: string, chainSelector: string }> = {
+  arbitrumSepolia: {
+    router: "0x2a9C5afB0d0e4BAb2BCdaE109EC4b0c4Be15a165",
+    usdc: "0x82A7176a7601764af75CC863640544f4B0ba8e43",
+    chainSelector: "3478487238524512106",
+  },
+  baseSepolia: {
+    router: "0xD3b06cEbF099CE7DA4AcCf578aaebFDBd6e88a93",
+    usdc: "0xD353131F4802046eF0f57FE362c64e641Be003Ad",
+    chainSelector: "10344971235874465080",
+  },
+  polygonAmoy: {
+    router: "0x9C32fCB86BF0f4a1A8921a9Fe46de3198bb884B2",
+    usdc: "0x41E94Eb019C0762f9Bfcf9Fb1E58725BfB0e7582",
+    chainSelector: "16281711391670634445",
+  },
+  sepolia: {
+    router: "0x0BF3dE8c5D3e8A2B34D2BEeB17ABfCeBaf363A59",
+    usdc: "0x854edF78e05Cd554CE538DA198Ce31807F2Cb7CF",
+    chainSelector: "16015286601757825753",
+  },
 };
 
-export const CCIP_ROUTER_ADDRESSES = Object.keys(ROUTER_ADDRESSES);
+export const CCIP_SUPPORTED_CHAINS = Object.keys(CHAIN_ADDRESSES);
